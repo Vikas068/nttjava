1867ab5696824b1faca6f10d8601448e8b778a86
Vikas068 <61828908+Vikas068@users.noreply.github.com>
2020-10-01 16:55:09 +0530

    Update LicMain.java

--- VIKAS PATIL_JDBCAPPLICATION3_201637/casestudy/JDBCApplication3_LIC/src/com/lic_application/LicMain.java
+++ VIKAS PATIL_JDBCAPPLICATION3_201637/casestudy/JDBCApplication3_LIC/src/com/lic_application/LicMain.java
@@ -4,13 +4,15 @@ import java.util.*;
 
 public class LicMain {
 	static int ch=0,result;
+	//Setting th scanner for input operation.
 	static Scanner scan=new Scanner(System.in);
+	//Main method.
 	public static void main(String[] args) {
 		
-	
+	//try/catch clause.
 	try {
 		do {
-			
+			//Operations to be performed.
 			System.out.println("-------Choose below one---------");
 			System.out.println("1.Insert Details");
 			System.out.println("2.Fetch Details");
@@ -18,24 +20,32 @@ public class LicMain {
 			System.out.println("4.Update details");
 			
 			System.out.println("Please choose any one opertaion to perform.");
+			//Reading choice from the user.
 			ch=scan.nextInt();
+			//Switch operation to switch reader choice to perticular case.
 			switch(ch)
 			{
 			case 1:
+					//insert method.
 				result=Lic.Insert();
 				break;
 			case 2:
+					//Fetch method.
 				Lic.Fetch();
 				break;
 			case 3:
+					//Delete method.
 				Lic.Delete();
 			case 4:
+					//Update method.
 				Lic.Update();
 			}
 			
 		}while(ch != 0);
+		//For the wrong choice below one is print.
 		System.out.println("Wrong selection...");
 	}
+		//Handling the exception.
 		catch(Exception e)
 		{
 			System.out.println(e);
